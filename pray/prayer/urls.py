b74d9caf7287ac43c1eaed64cecb25223d3a2f0b
James Avery King <avery492828+github@gmail.com>
2022-03-07 19:13:52 -0500

    Getting this done -- updating urls.py and views.py in initial integration to single django app

---
+++ pray/prayer/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	#Login Page and Troubleshooting
+	path('', views.login, name = 'Login'),
+	path('help/', views.help, name = 'Login Help'),
+	path('emailhelp/', views.emailhelp, name = 'Email Help'),
+	path('passwordhelp/', views.passwordhelp, name = 'Password Help'),
+	path('new/', views.new, name = 'New User')
+	#Individual User View URLs
+	path('<int:user_id>/prayers/', views.prayerrequests, name = 'View Prayer Requests'),
+	path('<int:user_id>/new/', views.prayerrequest, name='Submit Prayer Request'),
+	path('<int:user_id>/edit/<int:prayer_id>', views.prayeredit, name='Edit Prayer Request'),
+	path('<int:user_id>/security/', views.security, name = 'Security Settings'),
+	path('success/', views.success, name="Prayer Received"),
+	#Community URLs
+	path('home/', views.home, name = 'Home'),
+	path('latest/', views.latest, name = 'Latest Requests'),
+	path('prayedfor/', views.mostprayedfor, name='Most Prayed For'),
+	path('answered/', views.recentlyanswered, name='Answered Prayers'),
+	path('<int:prayer_id>/', views.prayer, name='Prayer'),
+	path('<int:prayer_id>/success/', views.latest, name='Successful Prayer'),
+	path('<int:user_id>/<int:prayer_id>/pray/', views.pray, name='Pray')
+]
---
+++ pray/prayer/views.py
@@ -0,0 +1,273 @@
+from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+from .models import Prayer
+from .forms import forms
+from django.template import loader
+
+
+#Create your views here.
+
+
+#Login and Login Help Views
+def login():
+	if request.method == 'GET':
+		form = forms.Login()
+		template = loader.get_template('form.html')
+		context = {
+			'action' : 'login'
+		}
+	elif request.method == 'POST':
+		form = forms.Login(request.POST)
+		if form.isvalid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			if valid('username',username):
+				if valid('password',password):
+					form.save()
+				else:
+					template = loader.get_template('error.html')
+					context = {
+						'errorL' : 'Incorrect Password'
+					}
+			else:
+				template = loader.get_template('error.html')
+				context = {
+					'errorL' : 'Username Not Located'
+				}
+			
+		else:
+			template = loader.get_template('error.html')
+			context = {
+				'errorF' : '501p Invalid Form'
+			}
+	else:
+		template = loader.get_template('error.html')
+		context = {
+			'errorP' : 'Invalid Protocol'
+		}
+	return HttpResponse(template.render(context,request))
+#def help():
+
+#def new():
+
+#def emailhelp():
+
+#def passwordhelp():
+
+
+
+
+#Individual User Views
+
+def prayerrequests(request):
+	#Load completed and current as separate lists
+	user_current_prayer_list = Prayer.objects.all().order_by('prayer_request_date')
+	user_answered_prayer_list = Prayer.objects.all().order_by('-prayer_request_date')
+	template = loader.get_template('viewprayerrequests.html')
+	context = {
+		'prayer_list' : user_current_prayer_list,
+		'answered_prayer_list': user_answered_prayer_list
+	}
+	return HttpResponse(template.render(context,request))
+	
+
+def prayerrequest(request):
+	if request.method == 'GET':
+		form = forms.PrayerRequestForm()
+		return render(request, 'prayerrequestform.html',{'form': form})
+	elif request.method == 'POST':
+		#prayer_request_date = request.POST['prayer_request_date']
+		#prayer_answer_date = request.POST['prayer_answer_date']
+		#prayer_description = request.POST['prayer_description']
+		#prayer_recipients = request.POST['prayer_recipients']
+		#prayer_recipients_email = request.POST['prayer_recipients_email']
+		#prayer_categories = request.POST['prayer_categories']
+		#prayer_answered = request.POST['prayer_answered']
+		#prayer_updates = request.POST['prayer_updates']
+		#prayer_image = request.POST['prayer_image']
+		#prayer_answered_image = request.POST['prayer_answered_image']
+		#validate each field in some way to account for SQL injection, etc.
+		form = forms.PrayerRequestForm(request.POST)
+		print(form.errors)
+		if form.is_valid():
+			#prayer = form.save(commit=False)
+			#prayer.prayer_request_date = request.prayer_request_date
+			#prayer.prayer_answer_date = request.prayer_answer_date
+			#prayer.prayer_description = request.prayer_description
+			#prayer.prayer_recipients = request.prayer_recipients
+			#prayer.prayer_recipients_email = request.prayer_recipients_email
+			#prayer.prayer_categories = request.prayer_categories
+			#prayer.prayer_answered = request.prayer_answered
+			#prayer.prayer_updates = request.prayer_updates
+			#prayer.prayer_image = request.prayer_image
+			#prayer.prayer_answered_image = request.prayer_answered_image
+			form.save()
+			return redirect('/success')
+		else:
+			context = {
+				'error' : '501pri'
+			}
+			template = loader.get_template('error.html')
+			return HttpResponse(template.render(context,request))
+
+def prayeredit(request,prayer_id):
+	if request.method == 'GET':
+		prayer = Prayer.objects.get(prayer_id=prayer_id)
+		#form = forms.PrayerRequestEditForm()
+		#PrayerFormSet = formset_factory(Prayer.objects.get(prayer_id=prayer_id),fields=('prayer_description','prayer_recipients','prayer_recipients_email','prayer_updates'))
+		PrayerFormSet = formset_factory(forms.PrayerRequestEditForm)	
+		formset = PrayerFormSet(initial=[
+			{
+				'prayer_description':prayer.prayer_description,
+				'prayer_recipients':prayer.prayer_recipients,
+				'prayer_recipients_email':prayer.prayer_recipients_email,
+				'prayer_updates':prayer.prayer_updates
+			}	
+		])
+		context = {
+			'form' : formset,
+			'prayer' : prayer,
+		}
+		template = loader.get_template('editrequestform.html')
+		print(prayer.prayer_description)
+		return HttpResponse(template.render(context,request))
+	elif request.method == 'POST':
+		#prayer_request_date = request.POST['prayer_request_date']
+		#prayer_answer_date = request.POST['prayer_answer_date']
+		#prayer_description = request.POST['prayer_description']
+		#prayer_recipients = request.POST['prayer_recipients']
+		#prayer_recipients_email = request.POST['prayer_recipients_email']
+		#prayer_categories = request.POST['prayer_categories']
+		#prayer_answered = request.POST['prayer_answered']
+		#prayer_updates = request.POST['prayer_updates']
+		#prayer_image = request.POST['prayer_image']
+		#prayer_answered_image = request.POST['prayer_answered_image']
+		#validate each field in some way to account for SQL injection, etc.
+		#form = forms.PrayerRequestEditForm(request.POST)
+		#prayer = Prayer.objects.get(prayer_id=prayer_id)
+		PrayerFormSet = formset_factory(Prayer,fields=('prayer_description','prayer_recipients','prayer_recipients_email','prayer_updates','prayer_id'),absolute_max=2,max_num=2,min_num=0)
+		prayerFilter = Prayer.objects.filter(prayer_id=prayer_id)
+		data = {
+			'form-TOTAL-FORMS':'2',
+			'form-INITIAL-FORMS':'0',
+			'queryset':prayerFilter,
+		}
+		formset = PrayerFormSet(request.POST,request.FILES,data)
+		print(str(formset))
+		if formset.is_valid():
+			#prayer = form.save(commit=False)
+			#prayer_request_date = form.cleaned_data.get('prayer_request_date')
+			#prayer_answer_date = form.cleaned_data.get('prayer_answer_date')
+			#prayer_description = form.cleaned_data.get('prayer_description')
+			#prayer_recipients = form.cleaned_data.get('prayer_recipients')
+			#prayer_recipients_email = form.cleaned_data.get('prayer_recipients_email')
+			#prayer_categories = form.cleaned_data.get('prayer_categories')
+			#prayer_answered = form.cleaned_data.get('prayer_answered')
+			#prayer_updates = form.cleaned_data.get('prayer_updates')
+			#prayer_image = form.cleaned_data.get('prayer_image')
+			#prayer_answered_image = form.cleaned_data.get('prayer_answered_image')
+			formset.save()
+			return redirect('/success')
+		else:
+			context = {
+				'error' : 501,
+				'formset' : formset
+			}
+			template = loader.get_template('error.html')
+			return HttpResponse(template.render(context,request))
+
+#def security(request, user_id):
+
+#def success(request, user_id, prayer_id):
+
+#Community and Pray Views
+def home(request):
+	template = loader.get_template('home.html')
+	context = {}
+	return HttpResponse(template.render(context,request))
+
+def latest(request):
+	#Retrieve top 20 latest prayers -- modify retrieval 
+	latest_prayer_list = Prayer.objects.all()
+	template = loader.get_template('prayerboard.html')
+	context = {
+		'prayer_list' : latest_prayer_list
+	}
+	return HttpResponse(template.render(context,request))
+
+def mostprayedfor(request):
+        #Retrieve top 20 most active prayers -- modify retrieval 
+        most_prayer_list = Prayer.objects.all()
+        template = loader.get_template('prayerboard.html')
+        context = {
+                'prayer_list' : most_prayer_list
+        }
+        return HttpResponse(template.render(context,request))
+
+def recentlyanswered(request):
+        #Retrieve top 20 latest answered prayers -- modify retrieval 
+        recent_prayer_list = Prayer.objects.all()
+        template = loader.get_template('prayerboard.html')
+        context = {
+                'prayer_list' : recent_prayer_list
+        }
+        return HttpResponse(template.render(context,request))
+
+def prayer(request,prayer_id):
+	#View an individual prayer to pray for it
+	if request.method == 'GET':
+		try:
+			prayer = Prayer.objects.get(pk=prayer_id)
+			print(prayer.prayer_description)
+			form = forms.PrayerRequest
+			template = loader.get_template('prayerrequest.html')
+			context = {
+				'prayer' : prayer,
+				'form' : form
+			}
+			return HttpResponse(template.render(context,request))
+		except Prayer.DoesNotExist:
+			raise Http404("Prayer ID is not in database")
+	else:
+		context = {
+			'error' : '501pv' #501 prayer view error
+		}
+		template = loader.get_template('error.html')
+		return HttpResponse(template.render(context,request))
+
+def pray(request, prayer_id, user_id):
+	if request.method == 'GET':
+		try:
+			prayer = Prayer.objects.get(pk=prayer_id)
+			form = forms.PrayView
+			template = loader.get_template('pray.html')
+			context = {
+				'prayer_title' : prayer.prayer_title,
+				'prayer_id' : prayer.prayer_id,
+				'form' : form
+			}
+			return HttpResponse(template.render(context,request))
+		except Prayer.DoesNotExist:
+			raise Http404('Prayer ID is not in database')
+	elif request.method == 'POST':
+		try:
+			prayer = Prayer.objects.get(pk=prayer_id)
+			form = forms.Pray(request.POST)
+			if form.isvalid():
+				#potentially reference other fields in prayer object to populate the rest of the form
+				form.save()
+			else:
+				context = {
+					'error' : '501pif' #501 prayer input form error
+				}
+				template = loader.get_template('error.html')
+				return HttpResponse(template.render(context,request))
+		except Prayer.DoesNotExist:
+			raise Http404("Prayer ID is not in database")
+	else:
+		context = {
+			'error' : '501p' #501 pray error
+		}
+		template = loader.get_template('error.html')
+		return HttpResponse(template.render(context,request))
